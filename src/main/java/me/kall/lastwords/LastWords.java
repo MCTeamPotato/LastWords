0e80a435edf427f5b3c0661304154d55139a9972
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-11-09 22:53:01 +0800

    port to fabric

--- src/main/java/me/kall/lastwords/LastWords.java
+++ src/main/java/me/kall/lastwords/LastWords.java
@@ -1,6 +1,8 @@
 package me.kall.lastwords;
 
 import me.kall.lastwords.ext.DongZhuo;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -8,31 +10,26 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(LastWords.MOD_ID)
-public final class LastWords {
+public class LastWords implements ModInitializer {
     public static final String MOD_ID = "lastwords";
     public static final String MOD_NAME = "LastWords";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
-    public LastWords() {
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, (LivingDamageEvent event) -> {
-            if (event.isCanceled()) return;
-            LivingEntity attacked = event.getEntity();
-            Entity attacker = event.getSource().getEntity();
-            if (attacker instanceof Player lvBu && event.getAmount() >= attacked.getHealth()) {
+    @Override
+    public void onInitialize() {
+        ServerLivingEntityEvents.ALLOW_DAMAGE.register((entity, source, amount) -> {
+            LivingEntity attacked = entity;
+            Entity attacker = source.getEntity();
+            if (attacker instanceof Player lvBu && amount >= attacked.getHealth()) {
                 if (attacked instanceof Player || attacked.isAlliedTo(lvBu)) {
+                    //if (attacked.level().isClientSide()) return;
                     if (((DongZhuo)attacked).lastWords$said()) {
                         ((DongZhuo)attacked).lastWords$setSaid(false);
-                        return;
+                        //return;
                     }
-                    if (attacked.level().isClientSide()) return;
 
                     Component lvBuWords = Component.literal("<" + lvBu.getName().getString() + "> " + I18n.get("lv_bu.last_words"));
                     Component dongZhuoWords = Component.literal("<" + attacked.getName().getString() + "> " + I18n.get("dong_zhuo.last_words"));
@@ -45,13 +42,15 @@ public final class LastWords {
                         dongZhuo.displayClientMessage(dongZhuoWords, false);
                     }
 
-                    event.setAmount(attacked.getHealth() - 1F);
+                    amount = attacked.getHealth() - 1F;
+                    //event.setAmount(attacked.getHealth() - 1F);
                     attacked.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, -1, 120, false, false, false));
                     attacked.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, -1, 120, false, false, false));
 
                     ((DongZhuo) attacked).lastWords$setSaid(true);
                 }
             }
+            return true;
         });
     }
 }
