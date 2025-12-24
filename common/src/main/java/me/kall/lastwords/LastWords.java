5863583624ed7cece62bbc1e072ada461b0e7d86
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-12-24 17:38:36 +0800

    fix crash on server

--- common/src/main/java/me/kall/lastwords/LastWords.java
+++ common/src/main/java/me/kall/lastwords/LastWords.java
@@ -1,6 +1,9 @@
 package me.kall.lastwords;
 
 import me.kall.lastwords.config.JsonConfig;
+import net.minecraft.network.chat.Component;
+
+import java.util.function.Supplier;
 
 public final class LastWords {
     public static final String MOD_ID = "lastwords";
@@ -8,6 +11,9 @@ public final class LastWords {
     public static JsonConfig CONFIG;
     public static boolean effect;
     public static int duration, amplifier;
+
+    public static final Supplier<String> LV_BU = () -> Component.translatable("lv_bu.last_words").getString();
+    public static final Supplier<String> DONG_ZHUO = () -> Component.translatable("dong_zhuo.last_words").getString();
 
     public static void init() {
         CONFIG = JsonConfig.create(MOD_ID, "1")
